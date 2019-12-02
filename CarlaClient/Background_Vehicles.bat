06e0f4aab472e0db4e46ff88614e05fef38c057b
Zealoft <simon0628@163.com>
2019-12-02 14:48:34 +0800

    final_displayed version

---
+++ CarlaClient/Background_Vehicles.bat
@@ -0,0 +1,2 @@
+cd /D D:\zwh\Carla_SUMO\CarlaClient\PythonAPI\examples
+python automatic_control.py & python automatic_control.py
--- CarlaClient/PythonAPI/examples/automatic_control.py
+++ CarlaClient/PythonAPI/examples/automatic_control.py
@@ -135,8 +135,13 @@ class World(object):
         cam_index = self.camera_manager.index if self.camera_manager is not None else 0
         cam_pos_index = self.camera_manager.transform_index if self.camera_manager is not None else 0
         # Get a random blueprint.
-        blueprint = random.choice(self.world.get_blueprint_library().filter(self._actor_filter))
-        # blueprint = self.world.get_blueprint_library().find('vehicle.*')
+        #blueprint = random.choice(self.world.get_blueprint_library().filter(self._actor_filter))
+        blueprints = self.world.get_blueprint_library().filter("vehicle.physx.*")
+        blueprint = None
+
+        while blueprint is None or int(blueprint.get_attribute('number_of_wheels')) == 2:
+            blueprint = random.choice(blueprints)
+        # blueprint = self.world.get_blueprint_library().find('Physx Citroen C3')
         blueprint.set_attribute('role_name', 'hero')
         if blueprint.has_attribute('color'):
             color = random.choice(blueprint.get_attribute('color').recommended_values)
@@ -1046,7 +1051,7 @@ def main():
     argparser.add_argument(
         '-s', '--speed',
         metavar='S',
-        default=20,
+        default=40,
         type=int,
         help='target speed of the vehicle(default: 20)')
     argparser.add_argument(
@@ -1057,7 +1062,7 @@ def main():
     argparser.add_argument(
         '--res',
         metavar='WIDTHxHEIGHT',
-        default='640x480',
+        default='960x540',
         help='window resolution (default: 1280x720)')
 
     argparser.add_argument("-a", "--agent", type=str,
